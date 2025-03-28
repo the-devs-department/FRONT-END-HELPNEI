6d84712bd9fe7e9cf021982df81c25cfb50ed75c
IssamiU <issami.umeoka@gmail.com>
2025-03-28 17:41:36 -0300

    Melhorias na responsividade: Ajustes de posicionamento absoluto, tamanhos de ícones e centralização de elementos para dispositivos menores. Adicionado suporte para telas abaixo de 362px.

--- src/containers/Index.tsx
+++ src/containers/Index.tsx
@@ -37,10 +37,10 @@ const App: React.FC = () => {
     <div>
 
     <br></br>
-
+  
     <div className="max-w-4xl mx-auto p-6 pb-2 bg-white shadow-lg rounded-lg">
       <div className="flex items-center justify-around border-b pb-4 gap-x-6">
-        <img src="/img/amazonlogopreta.png" alt="Amazon" className="w-[200px] h-[200px]" />
+        <img src="/img/amazonlogopreta.png" alt="Amazon" className="logo w-[200px] h-[200px]" />
         <div className="flex flex-col items-center">
           <p className="text-gray-700 text-3xl leading-relaxed font-bold">Amazon</p>
           <p className="text-gray-700 text-1xl leading-relaxed pb-3">
@@ -52,12 +52,12 @@ const App: React.FC = () => {
       <h2 className="text-2xl font-bold mt-6 text-center border-b pb-6">IMPACTO DA EMPRESA</h2>
  
       <div className="grid grid-cols-2 gap-4 mt-6">
-        <div className="bg-gray-300 p-10 shadow-lg rounded-lg text-center flex items-center justify-around col-span-2 gap-x-6 transition-transform transform hover:scale-105 cursor-pointer">
+        <div className="cidade bg-gray-300 p-10 shadow-lg rounded-lg text-center flex items-center justify-around col-span-2 gap-x-6 transition-transform transform hover:scale-105 cursor-pointer">
           <div className="absolute top-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
           <img src="/img/mapaicon.png" alt="Ícone" className="w-24 h-24" />
-          <div className="flex flex-col items-center">
+          <div className="cidadetext flex flex-col items-center">
             <p className="text-gray-800 text-[50px] font-bold counter" data-count="125000">0</p>
-            <p className="font-semibold text-[23px]">CIDADES ATENDIDAS</p>
+            <p className="font-semibold text-[20px]">CIDADES ATENDIDAS</p>
           </div>
           <img src="/img/mapaicon.png" alt="Ícone" className="w-24 h-24" />
           <div className="absolute bottom-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
@@ -65,46 +65,46 @@ const App: React.FC = () => {
  
         <div className="bg-gray-300 p-10   rounded-lg shadow-md transition-transform transform hover:scale-105 cursor-pointer">
           <div className="absolute top-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
-          <img src="/img/usuarioicon.png" alt="Ícone" className="w-32 h-32 absolute bottom-6 right-9" />
+          <img src="/img/usuarioicon.png" alt="Ícone" className="w-32 h-32 custom-absolute" />
           <div className="text-left">
             <p className="text-gray-800 text-[50px] font-bold counter" data-count="1200">0</p>
-            <p className="font-semibold text-[23px]">USUÁRIOS TOTAIS</p>
+            <p className="font-semibold text-[17px]">USUÁRIOS TOTAIS</p>
           </div>
           <div className="absolute bottom-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
         </div>
  
         <div className="bg-gray-300 p-10 rounded-lg shadow-md transition-transform transform hover:scale-105 cursor-pointer">
           <div className="absolute top-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
-          <img src="/img/afiliados.png" alt="Ícone" className="w-32 h-32 absolute bottom-6 right-9" />
+          <img src="/img/afiliados.png" alt="Ícone" className="w-32 h-32 custom-absolute" />
           <div className="text-left">
             <p className="text-gray-800 text-[50px] font-bold counter" data-count="800">0</p>
-            <p className="font-semibold text-[23px]">AFILIADOS TOTAIS</p>
+            <p className="font-semibold text-[17px]">AFILIADOS TOTAIS</p>
           </div>
           <div className="absolute bottom-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
         </div>
  
         <div className="bg-gray-300 p-10 rounded-lg shadow-md transition-transform transform hover:scale-105 cursor-pointer">
           <div className="absolute top-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
-          <img src="/img/graficoicon.png" alt="Ícone" className="w-24 h-24 absolute bottom-11 right-9" />
+          <img src="/img/graficoicon.png" alt="Ícone" className="w-24 h-24 custom-absolute2" />
           <div className="text-left">
             <p className="text-gray-800 text-[50px] font-bold counter" data-count="75" data-suffix="%">0%</p>
-            <p className="font-semibold text-[23px]">CRESCIMENTO MÉDIO</p>
+            <p className="font-semibold text-[17px]">CRESCIMENTO MÉDIO</p>
           </div>
           <div className="absolute bottom-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
         </div>
  
         <div className="bg-gray-300 p-10 rounded-lg shadow-md flex justify-start relative transition-transform transform hover:scale-105 cursor-pointer">
           <div className="absolute top-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
-          <img src="/img/shopping-store.png" alt="Ícone" className="w-32 h-32 absolute bottom-8 right-10" />
+          <img src="/img/shopping-store.png" alt="Ícone" className="w-32 h-32 custom-absolute" />
           <div className="text-left">
             <p className="text-gray-800 text-[50px] font-bold counter" data-count="150">0</p>
-            <p className="font-semibold text-[23px]">LOJAS CRIADAS</p>
+            <p className="font-semibold text-[17px]">LOJAS CRIADAS</p>
           </div>
           <div className="absolute bottom-3 left-3 right-3 h-1 bg-blue-950 rounded-full"></div>
         </div>
  
-        <div className="bg-gray-300 p-6 rounded-lg shadow-md text-center justify-around flex items-center col-span-2 relative ">
-        <div className="flex flex-col items-center justify-center gap-y-4">
+        <div className="mapa bg-gray-300 p-6 rounded-lg shadow-md text-center justify-around flex items-center col-span-2 relative ">
+        <div className="mapatext flex flex-col items-center justify-center gap-y-4">
             <p className="text-gray-800 text-8xl font-bold counter" data-count="5569">0</p>
             <p className="font-semibold text-2xl">CIDADES ATINGIDAS</p>
             <p className="text-lg text-[#143357] font-bold">Estado: {selectedState ? selectedState : "--"}</p>
@@ -124,7 +124,7 @@ const App: React.FC = () => {
         </div>
       </div>
  
-      <div className="flex items-center justify-center pt-4 gap-x-12 col-span-2">
+      <div className="social flex items-center justify-center pt-4 gap-x-12 col-span-2">
         <a href="https://www.linkedin.com/company/amazon/posts/?feedView=all" target="_blank">
           <img src="img/linkedin.png" alt="Ícone" className="w-9 h-9 transition-transform transform hover:scale-110 cursor-pointer" />
         </a>
@@ -135,8 +135,8 @@ const App: React.FC = () => {
           <img src="img/www.png" alt="Ícone" className="w-9 h-9 transition-transform transform hover:scale-110 cursor-pointer" />
         </a>
       </div>
- 
-      <div className="w-full max-w-none text-center mt-3 border-t pt-2 pb-0" style={{ width: "calc(100% + 48px)", marginLeft: "-24px" }}>
+  
+      <div className="w-full max-w-none text-center mt-3 border-t pt-2 pb-0">
         <p className="text-gray-600 font-semibold">Helpnei</p>
       </div>
     </div>
