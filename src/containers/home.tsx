2b58bc9b8a17c7378e9efe43b324fd672ec75dc8
Nic <guedesdeabreunicoly@gmail.com>
2025-04-22 21:50:20 -0300

    add pctr

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import mercedes from '../images/mercedes.png';
+import audi from '../images/audi.png';
+import cunzolo from '../images/cunzolo.png';
+import johnson from '../images/johnson.png';
+import bmw from '../images/bmw.png';
+import heineken from '../images/heineken.png';
+import nike from '../images/nike.png';
+import porsche from '../images/porsche.png';
 
 const Home = () => {
   const empresas = [
-    "Mercedes-Benz",
-    "Audi",
-    "Cunzolo",
-    "Johnson & Johnson",
-    "BMW",
-    "Heineken",
-    "Nike",
-    "Porsche",
+    { nome: 'Mercedes-Benz', logo: mercedes },
+    { nome: 'Audi', logo: audi },
+    { nome: 'Cunzolo', logo: cunzolo },
+    { nome: 'Johnson & Johnson', logo: johnson },
+    { nome: 'BMW', logo: bmw },
+    { nome: 'Heineken', logo: heineken },
+    { nome: 'Nike', logo: nike },
+    { nome: 'Porsche', logo: porsche },
   ];
 
   return (
@@ -17,10 +24,10 @@ const Home = () => {
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 justify-items-center">
         {empresas.map((empresa) => (
           <div
-            key={empresa}
-            className="bg-white shadow-md rounded-lg p-4 w-36 h-20 flex items-center justify-center text-center font-medium text-gray-800"
+            key={empresa.nome}
+            className="bg-white shadow-md rounded-lg p-4 w-36 h-20 flex items-center justify-center"
           >
-            {empresa}
+            <img src={empresa.logo} alt={empresa.nome} className="max-h-full max-w-full object-contain" />
           </div>
         ))}
       </div>
@@ -31,4 +38,5 @@ const Home = () => {
     </div>
   );
 };
+
 export default Home;
