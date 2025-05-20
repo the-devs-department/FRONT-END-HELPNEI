d1c9e0460c293aca7d7f69af26a25af73cec95f7
nicoly <guedesdeabreunicoly@gmail.com>
2025-05-20 10:49:50 -0300

    feat(gh-81)
    atualização da página home

--- src/containers/HomePage/Home.tsx
+++ src/containers/HomePage/Home.tsx
@@ -1,50 +1,55 @@
-import React from "react";
-import LogoHelpnei from '../../images/logoHelpnei.webp'
-import styles from './styles.module.css'
-import mercedes from '../../images/mercedes.png'
-import audi from '../../images/audi.png'
-import cunzolo from '../../images/cunzolo.png'
-import johnson from '../../images/johnson.png'
-import bmw from '../../images/bmw.png'
-import heineken from '../../images/heineken.png'
-import nike from '../../images/nike.png'
-import porsche from '../../images/porsche.png'
-import amazon from '../../images/amazonlogopreta.png'
+import React, { useEffect, useState } from "react";
+import LogoHelpnei from '../../images/logoHelpnei.webp';
+import styles from './styles.module.css';
 import { Link } from "react-router-dom";
-// import { Link } from "react-router-dom";
 
+interface Company {
+  sponsorId: number;
+  highSponsorLogo: string;
+}
 
-const Home :React.FC =  () => {
-  const empresas = [
-    { nome: 'Mercedes-Benz', logo: mercedes },
-    { nome: 'Audi', logo: audi },
-    { nome: 'Cunzolo', logo: cunzolo },
-    { nome: 'Johnson & Johnson', logo: johnson },
-    { nome: 'BMW', logo: bmw },
-    { nome: 'Heineken', logo: heineken },
-    { nome: 'Nike', logo: nike },
-    { nome: 'Porsche', logo: porsche },
-    {nome: 'Amazon', logo: amazon}
-  ];
+const Home: React.FC = () => {
+  const [empresas, setEmpresas] = useState<Company[]>([]);
+
+  useEffect(() => {
+    const fetchEmpresas = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/companies");
+        const data = await response.json();
+
+        // Filtra só os que têm logo
+        const comLogo = data.filter((empresa: any) => empresa.highSponsorLogo);
+        setEmpresas(comLogo);
+      } catch (error) {
+        console.error("Erro ao buscar empresas:", error);
+      }
+    };
+
+    fetchEmpresas();
+  }, []);
 
   return (
     <>
       <div className="w-full h-full flex flex-col justify-between">
         <div className="w-full h-[90%] flex flex-col items-center justify-center">
-          <div className={styles.company_group} key={1}>
+          <div className={styles.company_group}>
             {empresas.map((empresa) => (
-                <Link
+              <Link
                 to={'/dashboard'}
-                key={empresa.nome}
-                className={`":max-lg:w-[10rem] ${styles.company} shadow-lg bg-white shadow-md rounded-lg w-[25rem] h-[10rem] text-center flex items-center justify-center p-4 cursor-pointer"`}
-                >
-                  <img src={empresa.logo} alt={empresa.nome} className={styles.company_logo}/>
-                </Link>
+                key={empresa.sponsorId}
+                className={`max-lg:w-[10rem] ${styles.company} shadow-lg bg-white rounded-lg w-[25rem] h-[10rem] text-center flex items-center justify-center p-4 cursor-pointer`}
+              >
+                <img
+                  src={empresa.highSponsorLogo}
+                  alt={`Sponsor ${empresa.sponsorId}`}
+                  className={styles.company_logo}
+                />
+              </Link>
             ))}
           </div>
         </div>
         <footer className="flex justify-center items-center w-full h-12 fixed bottom-0 left-0 bg-blue-950 text-white py-4">
-            <img src={LogoHelpnei} alt="" className='h-[100%] object-contain'/>
+          <img src={LogoHelpnei} alt="Logo Helpnei" className='h-[100%] object-contain' />
         </footer>
       </div>
     </>
